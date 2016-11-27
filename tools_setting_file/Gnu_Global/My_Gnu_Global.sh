60f528e4eefe5fa30ef7d41c578fc57902cbccba
xxx <youchihwang@gmail.com>
2016-11-27 18:09:51 +0800

    add gnu global relating shell script

---
+++ tools_setting_file/Gnu_Global/My_Gnu_Global.sh
@@ -0,0 +1,4 @@
+find linux_repo/vendor/mediatek/proprietary/ ! -type d -not -iwholename '*/.git*' >  gtags.files
+find linux_repo/kernel-3.18/drivers/ ! -type d -not -iwholename '*/.git*'         >> gtags.files
+find linux_repo/kernel-3.18/arch/arm64/ ! -type d -not -iwholename '*/.git*'      >> gtags.files
+find linux_repo/device/mediatek/ ! -type d -not -iwholename '*/.git*'             >> gtags.files
