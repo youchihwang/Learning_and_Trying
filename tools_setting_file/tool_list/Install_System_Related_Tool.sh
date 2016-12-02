d112c67ea3406730313b876fedc3a653d90d729a
youchihwang <youchihwang@gmail.com>
2016-12-02 16:02:16 +0800

    Add tools list neets to be installed

---
+++ tools_setting_file/tool_list/Install_System_Related_Tool.sh
@@ -0,0 +1,17 @@
+sudo apt-get -y install vim
+
+
+install  google chrome
+
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+
+adjust keyboard speed
+
+
+
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo apt-get update
+sudo apt-get install fluxgui
+sudo apt-get install xclip
