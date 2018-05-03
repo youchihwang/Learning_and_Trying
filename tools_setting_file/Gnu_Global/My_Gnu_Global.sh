c76a8d03fe8e7322f731b2da040f384c41ee8f16
Youchihwang <youchihwang@gmail.com>
2018-05-03 13:01:14 +0000

    Update My_Gnu_Global.sh

--- tools_setting_file/Gnu_Global/My_Gnu_Global.sh
+++ tools_setting_file/Gnu_Global/My_Gnu_Global.sh
@@ -1,7 +1,7 @@
 #find linux_repo/vendor/mediatek/proprietary/ ! -type d ! -iwholename '*/.git*' >  gtags.files
 #find linux_repo/kernel-3.18/drivers/         ! -type d ! -iwholename '*/.git*' >> gtags.files
 #find linux_repo/kernel-3.18/arch/arm64/      ! -type d ! -iwholename '*/.git*' >> gtags.files
-find android/kernel/         ! -type d ! -iwholename '*/.git*' >> gtags.files
-find android/bootable/bootloader/lk/         ! -type d ! -iwholename '*/.git*' >> gtags.files
-find amss/BOOT.BF.3.3/       ! -type d ! -iwholename '*/.git*' >> gtags.files
+find android/kernel/                        ! -type d ! -iwholename '*/.git*' >> gtags.files
+find android/bootable/bootloader/lk/        ! -type d ! -iwholename '*/.git*' >> gtags.files
+find amss/BOOT.BF.3.3/                      ! -type d ! -iwholename '*/.git*' >> gtags.files
 #find linux_repo/kernel-3.18/         ! -type d ! -iwholename '*/.git*' >> gtags.files
